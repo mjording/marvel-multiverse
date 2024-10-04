4c5e3b20c7b7a5ca223fe2bbec9a31d4bb140a99
Matthew Jording <mjording@gmail.com>
2024-10-03 21:11:51 -0400

    removed unused damageroll

--- marvel-multiverse.mjs
+++ marvel-multiverse.mjs
@@ -81,13 +81,11 @@ Hooks.once('init', function () {
   Roll.TOOLTIP_TEMPLATE = "systems/marvel-multiverse/templates/chat/roll-breakdown.hbs";
   Roll.CHAT_TEMPLATE = "systems/marvel-multiverse/templates/dice/roll.hbs"
   CONFIG.Dice.MarvelMultiverseRoll = dice.MarvelMultiverseRoll;
-  // CONFIG.Dice.DamageRoll = dice.DamageRoll;
 
   // Register Roll Extensions
   CONFIG.Dice.rolls.push(dice.MarvelMultiverseRoll);
   CONFIG.Dice.terms.m = dice.MarvelDie;
-  //registerChatLog();
-  // CONFIG.Dice.rolls.push(dice.DamageRoll);
+
   // Add fonts
   _configureFonts();
 
